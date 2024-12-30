7252e379c45fcf00aec529c18e69c99940c18267
fathima-nihala <fathimanihala200214@gmail.com>
2024-12-31 00:13:53 +0530

    band generator

--- BAND GENERATOR/index.js
+++ BAND GENERATOR/index.js
@@ -11,6 +11,8 @@ const port = 3000;
 //Step 4 - Add a dynamic year to the footer.
 //Hint: Google to find out how to get the current year using JS.
 
+app.use(express.static("public"));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
@@ -19,6 +21,10 @@ app.get("/", (req, res) => {
 });
 
 app.post("/submit", (req, res) => {
+
+  const randomAdj = adj[Math.floor(Math.random()*adj.length)];
+  const randomNoun = noun[Math.floor(Math.random()* noun.length)];
+  res.render("index.ejs", {adjective: randomAdj, noun: randomNoun});
   //Step 2 - Make the generate name functionality work
   //Hint: When the "Generate Name" button in index.ejs is clicked, it should hit up this route.
   //Then:
